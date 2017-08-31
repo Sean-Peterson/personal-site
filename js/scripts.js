800111f2e895c187b686067a1d338e4286d9c1b6
SeanPeterson <seanpeterson11@gmail.com>
2017-08-31 11:02:31 -0500

    add base

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,8 @@
 
 
 $(document).ready(function() {
-  function showHidden(){
-    $(".hidden").fadeIn(3000).removeClass('hidden');
-  }
-  setTimeout(showHidden, 2000);
+  // function showHidden(){
+  //   $(".hidden").fadeIn(3000).removeClass('hidden');
+  // }
+  // setTimeout(showHidden, 2000);
 });
