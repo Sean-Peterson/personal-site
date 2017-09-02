80e2bd5f991928cdcb5638c69068a7e848f8ece1
SeanPeterson <seanpeterson11@gmail.com>
2017-09-02 14:23:47 -0500

    add pop up images

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 function showHidden(){
   $(".hidden").fadeIn(1500).removeClass('hidden');
 }
+function popUp(id,classes){
+  var element = document.getElementById(id);
+  var bool = element.classList.contains('pop');
+  if (!bool) {
+    element.classList.add("pop");
+  }else{
+    element.classList.remove("pop");
+  }
+}
 $(document).ready(function() {
   setTimeout(showHidden, 1000);
+  $(".images").click(function(event){
+    var id = $(this).attr('id');
+    var classes = $(this).attr('class')
+    popUp(id,classes);
+  });
 });
