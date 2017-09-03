dcd3697af35c6c2d8c0d5b57317e4752466b5dc0
SeanPeterson <seanpeterson11@gmail.com>
2017-09-03 13:35:27 -0500

    add images for icons and more images for pics. Fix styling, add footer

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,14 @@ function showHidden(){
   $(".hidden").fadeIn(1500).removeClass('hidden');
 }
 function popUp(id,classes){
-  var element = document.getElementById(id);
-  var bool = element.classList.contains('pop');
-  if (!bool) {
-    element.classList.add("pop");
-  }else{
-    element.classList.remove("pop");
-  }
+  var elements = Array.from(document.getElementsByClassName(classes)).forEach(function(e){
+    var bool = e.classList.contains('pop');
+    if (!bool) {
+      e.classList.add("pop");
+    }else{
+      e.classList.remove("pop");
+    }
+  });
 }
 $(document).ready(function() {
   setTimeout(showHidden, 1000);
