7bc90cd598d31d57fc82a5bb091d0529b5ef2a03
SeanPeterson <seanpeterson11@gmail.com>
2017-09-14 15:22:40 -0500

    change styling completely

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,7 @@
 function showHidden(){
-  $(".hidden").fadeIn(1500).removeClass('hidden');
+  $(".hidden").fadeIn(2000).removeClass('hidden');
 }
-function popUp(id,classes){
-  var elements = Array.from(document.getElementsByClassName(classes)).forEach(function(e){
-    var bool = e.classList.contains('pop');
-    if (!bool) {
-      e.classList.add("pop");
-    }else{
-      e.classList.remove("pop");
-    }
-  });
-}
+
 $(document).ready(function() {
   setTimeout(showHidden, 1000);
-  $(".images").click(function(event){
-    var id = $(this).attr('id');
-    var classes = $(this).attr('class')
-    popUp(id,classes);
-  });
 });
